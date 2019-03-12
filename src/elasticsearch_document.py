dcd323d8e57000a6f1fce562701427aaa74e60be
Sapan Gupta <sapangupta89@gmail.com>
2019-03-12 17:56:18 +0530

    adding year and quarter for report

--- src/elasticsearch_document.py
+++ src/elasticsearch_document.py
@@ -3,6 +3,7 @@
 
 """
 import json
+from datetime import datetime
 
 class ElasticSearchDocument(object):
     def __init__(self):
@@ -19,6 +20,11 @@ class ElasticSearchDocument(object):
         self.document_type = doc_metadata.document_type
         self.cik = doc_metadata.sec_cik
         self.report_period = doc_metadata.sec_period_of_report
+        
+        report_period_date = datetime.strptime(doc_metadata.sec_period_of_report, '%Y%m%d')
+        self.report_year = report_period_date.year
+        quarter = ((report_period_date.month -1) // 3) + 1
+        self.report_quarter = 'Q' + str(quarter)
         self.doc_extension = doc_metadata.extraction_method
         self.doc_url = doc_metadata.sec_url
         self.doc_text = doc_text
